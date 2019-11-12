254820e8a2c96a71e14cacfd760788784a76b090
miyuesc <913784771@qq.com>
2019-11-12 22:46:10 +0800

    update

--- src/components/ChartModels/LineChart.vue
+++ src/components/ChartModels/LineChart.vue
@@ -12,7 +12,20 @@
 import { Vue, Component, Prop, Watch } from "vue-property-decorator";
 
 @Component({})
-export default class LineChart extends Vue {}
+export default class LineChart extends Vue {
+  @Prop({ type: String, default: "" })
+  height!: string;
+  @Prop({ type: String, default: "" })
+  width!: string;
+  @Prop({ type: Array })
+  colors!: string[];
+  @Prop({ type: String })
+  backgroundStyle!: string;
+  @Prop({ type: Number })
+  backgroundOpacity!: number;
+  @Prop({ type: String })
+  title!: string;
+}
 </script>
 
 <style scoped lang="less"></style>
